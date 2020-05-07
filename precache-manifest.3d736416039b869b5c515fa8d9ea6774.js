8fbb0aa1e845755b1c44567d5fde69cf04879d61
ThomasSuzuki <>
2020-05-07 22:13:25 +0900

    Updates

---
+++ precache-manifest.3d736416039b869b5c515fa8d9ea6774.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c30b36909f600ba12a22704f44f4c866",
+    "url": "/todo_react/index.html"
+  },
+  {
+    "revision": "2f69ecbe96c881e7b47f",
+    "url": "/todo_react/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "5fceb97a13e94369ccc1",
+    "url": "/todo_react/static/js/2.d0b9171d.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/todo_react/static/js/2.d0b9171d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2f69ecbe96c881e7b47f",
+    "url": "/todo_react/static/js/main.bae04283.chunk.js"
+  },
+  {
+    "revision": "93efb3ad9f55d8d2515f",
+    "url": "/todo_react/static/js/runtime-main.0cc21a2b.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/todo_react/static/media/logo.5d5d9eef.svg"
+  }
+]);
